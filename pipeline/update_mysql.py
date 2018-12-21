bbed0c81e96f9bd5129e548e934261962158a991
sangjing <sangjing@leinao.ai>
2018-12-21 17:15:14 +0800

    LNPAI 正式插入数据库

--- pipeline/update_mysql.py
+++ pipeline/update_mysql.py
@@ -8,7 +8,7 @@ imageName = sys.argv[1]
 imageNameSplit = imageName.split('/')
 provider = sys.argv[2]
 description = sys.argv[3]
-db = MySQLdb.connect("127.0.0.1", "root", "sangjing", "dataset", charset='utf8' )
+db = MySQLdb.connect("10.12.3.2", "root", "12345678", "dataset", charset='utf8' )
 cursor = db.cursor()
 sql = """INSERT INTO images(name,place,description,provider,createtime) VALUES ("%s","%s/%s","%s","%s",NOW())"""%(imageNameSplit[2],imageNameSplit[0],imageNameSplit[1],description,provider)
 try:
